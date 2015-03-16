2b6231a5712461487c91eaf29a072b13b81c351a
Caleb Lee <caleb.a.lee@icloud.com>
2015-03-16 13:11:58 -0500

    Implements management functions for Deck

--- Scheduled Flash Cards/Deck+Management.m
+++ Scheduled Flash Cards/Deck+Management.m
@@ -7,9 +7,29 @@
 //
 
 #import "Deck+Management.h"
+#import "DatabaseManager.h"
 
 @implementation Deck (Management)
-
 static NSString *entityName = @"Deck";
+
+// creates/inserts a new deck with the given name and returns it
++ (Deck*)insertDeckWithName:(NSString*)name {
+    Deck *newDeck = (Deck*)[NSEntityDescription insertNewObjectForEntityForName:entityName inManagedObjectContext:defaultManagedObjectContext()];
+    
+    return newDeck;
+}
+
+// grabs the deck with the given name out of the database and returns it
+//  returns nil if deck with no such name exists
++ (Deck*)deckWithName:(NSString*)name {
+    // set up predicate and sort descriptors
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"name == %@", name];
+    NSArray *sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"name" ascending:YES]];
+    
+    // fetch the deck with name
+    Deck *deck = (Deck*)fetchObject(entityName, predicate, sortDescriptors);
+    
+    return deck;
+}
 
 @end
