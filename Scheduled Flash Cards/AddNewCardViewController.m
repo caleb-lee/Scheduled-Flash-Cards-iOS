9f6c3cb285e8417c0eba99c60dfe2eb5f9de7ab5
Caleb Lee <caleb.a.lee@icloud.com>
2015-03-16 22:11:45 -0500

    Makes interface to add cards account for keyboard

--- Scheduled Flash Cards/AddNewCardViewController.m
+++ Scheduled Flash Cards/AddNewCardViewController.m
@@ -15,7 +15,6 @@
 - (IBAction)addCardButtonAction:(id)sender;
 
 // constraints to change when the keyboard shows
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *containerTopConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *containerBottomConstraint;
 
 // text views
@@ -32,6 +31,15 @@
     [_addCardButton addThinGreyBorder];
     [_frontTextView addThinGreyBorder];
     [_backTextView addThinGreyBorder];
+    
+    // register keyboard notifications
+    [self registerForKeyboardNotifications];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [_frontTextView becomeFirstResponder];
 }
 
 - (void)dealloc {
@@ -42,8 +50,29 @@
     [Card insertCardWithFront:_frontTextView.text Back:_backTextView.text intoDeck:_deck];
     _frontTextView.text = @"";
     _backTextView.text = @"";
+    [_frontTextView becomeFirstResponder];
     
     UIAlertView *successAlert = [[UIAlertView alloc] initWithTitle:nil message:@"Card added successfully" delegate:nil cancelButtonTitle:nil otherButtonTitles:@"OK", nil];
     [successAlert show];
 }
+
+// notifies when the keyboard is being shown or hidden
+// from https://developer.apple.com/library/ios/documentation/StringsTextFonts/Conceptual/TextAndWebiPhoneOS/KeyboardManagement/KeyboardManagement.html
+- (void)registerForKeyboardNotifications {
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeShown:)
+                                                 name:UIKeyboardWillShowNotification object:nil];
+}
+
+// the method called when the keyboard gets shown
+//  it changes containerViewBottomSpaceConstraint to the correct constant and lays out the vc's views
+//  from https://developer.apple.com/library/ios/documentation/StringsTextFonts/Conceptual/TextAndWebiPhoneOS/KeyboardManagement/KeyboardManagement.html
+- (void)keyboardWillBeShown:(NSNotification*)aNotification {
+    NSDictionary* info = [aNotification userInfo];
+    CGSize kbSize = [[info objectForKey:UIKeyboardFrameBeginUserInfoKey] CGRectValue].size;
+    
+    _containerBottomConstraint.constant = kbSize.height;
+    [self.view layoutSubviews];
+}
+
 @end
